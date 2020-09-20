51b9fc24a4d70990f81737f972e70c87bcdcc2a8
Gevorg <gevorg89@gmail.com>
2020-09-20 22:49:20 +0300

    setup

---
+++ app/src/main/java/com/example/mvvm_jetpack_second/data/entities/CharacterList.kt
@@ -0,0 +1,6 @@
+package com.example.mvvm_jetpack_second.data.entities
+
+data class CharacterList(
+    val info: Info,
+    val results: List<Character>
+)
---
+++ app/src/main/java/com/example/mvvm_jetpack_second/data/repository/CharacterRepository.kt
@@ -0,0 +1,24 @@
+package com.example.mvvm_jetpack_second.data.repository
+
+import com.example.mvvm_jetpack_second.data.local.CharacterDao
+import com.example.mvvm_jetpack_second.data.remote.CharacterRemoteDataSource
+import com.example.mvvm_jetpack_second.utils.performGetOperation
+import javax.inject.Inject
+
+class CharacterRepository @Inject constructor(
+    private val remoteDataSource: CharacterRemoteDataSource,
+    private val localDataSource: CharacterDao
+) {
+
+    fun getCharacter(id: Int) = performGetOperation(
+        databaseQuery = { localDataSource.getCharacter(id) },
+        networkCall = { remoteDataSource.getCharacter(id) },
+        saveCallResult = { localDataSource.insert(it) }
+    )
+
+    fun getCharacters() = performGetOperation(
+        databaseQuery = { localDataSource.getAllCharacters() },
+        networkCall = { remoteDataSource.getCharacters() },
+        saveCallResult = { localDataSource.insertAll(it.results) }
+    )
+}
